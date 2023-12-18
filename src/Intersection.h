76f2ff2135aa55125fd006fa11380b7e83815a35
Christoph Graßl <christoph.grassl@bshg.com>
2023-12-18 21:58:54 +0100

    first implementation

--- src/Intersection.h
+++ src/Intersection.h
@@ -6,6 +6,7 @@
 #include <mutex>
 #include <memory>
 #include "TrafficObject.h"
+#include "TrafficLight.h"
 
 // forward declarations to avoid include cycle
 class Street;
@@ -46,7 +47,6 @@ public:
     bool trafficLightIsGreen();
 
 private:
-
     // typical behaviour methods
     void processVehicleQueue();
 
@@ -54,6 +54,7 @@ private:
     std::vector<std::shared_ptr<Street>> _streets;   // list of all streets connected to this intersection
     WaitingVehicles _waitingVehicles; // list of all vehicles and their associated promises waiting to enter the intersection
     bool _isBlocked;                  // flag indicating wether the intersection is blocked by a vehicle
+    TrafficLight _trafficLight;
 };
 
 #endif
