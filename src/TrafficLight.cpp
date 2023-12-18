a7b8443b0e606d30f3ec31f29590ae8cc91947ec
Christoph Graßl <christoph.grassl@bshg.com>
2023-12-18 22:40:11 +0100

    fix bug when initializing unique_lock

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -10,7 +10,7 @@ T MessageQueue<T>::receive()
     // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
     // to wait for and receive new messages and pull them from the queue using move semantics. 
     // The received object should then be returned by the receive function. 
-    std::unique_lock<std::mutex> uLock;
+    std::unique_lock<std::mutex> uLock(_mutex);
     _condition.wait(uLock, [this] { return !_queue.empty(); });
 
     T msg = std::move(_queue.back());
@@ -26,7 +26,6 @@ void MessageQueue<T>::send(T &&msg)
     std::lock_guard<std::mutex> guard(_mutex);
 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
-    std::cout << "Message #" << msg << " will be added to the queue\n";
     _queue.push_back(std::move(msg));
     _condition.notify_one();
 }
